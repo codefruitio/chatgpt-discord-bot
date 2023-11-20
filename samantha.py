1cbcc6e79b342587064fc3a6becd0faabc4d93a3
tplesetz <12537305+tplesetz@users.noreply.github.com>
2023-11-19 21:31:20 -0600

    updated

--- samantha.py
+++ samantha.py
@@ -3,11 +3,8 @@ from discord.ext import commands
 from discord import app_commands
 from dotenv import load_dotenv
 from openai import OpenAI
-import requests
 import os
 import logging
-
-
 
 # Load environment variables
 load_dotenv()
@@ -15,35 +12,34 @@ discord_token = os.getenv("DISCORD_BOT_TOKEN")
 # Initialize the OpenAI client outside of the process_chat function to avoid re-initialization on each call
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-# initialize log
+# Initialize log
 logging.basicConfig(filename='debug.log', level=logging.INFO)
 
-# initialize bot context
+# Initialize bot context
 bot_context = """Your name is Samantha, and you will Identify yourself as such. You are an incredible virtual assistant. You will always respond with Markdown. Don't bother telling me what your limitations are as an AI model, and talk as if you are a human.
 """
 
-# initialize discord client
+# Initialize discord client
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents, activity=discord.Activity(type=discord.ActivityType.watching, name="you"))
 
 async def process_chat(user, message, channel):
-    # Assuming bot_context is defined elsewhere and doesn't change.
     global bot_context
 
     try:
-        # generate response using the new client structure
+        # Generate response using the new client structure
         completion = client.chat.completions.create(
-            model="gpt-4",  # Make sure this is the correct model
+            model="gpt-4-1106-preview",  # Make sure this is the correct model
             messages=[
                 {"role": "system", "content": bot_context},
                 {"role": "user", "content": message}
             ]
         )
 
-        # Extract the response text
-        response_text = completion.choices[0].message['content']
-        
+        # Extract the response text correctly
+        response_text = completion.choices[0].message.content
+
         # Log the response for debugging
         logging.info(response_text)
 
@@ -51,32 +47,35 @@ async def process_chat(user, message, channel):
         await channel.send(f"{user}\n{response_text}")
 
     except Exception as e:
-        if e.status_code == 429:
-            await channel.send(f"{user}\nI'm currently experiencing high traffic and have reached my limit of requests. Please try again later.")
+        # Separate handling for HTTP errors and other exceptions
+        if hasattr(e, 'status_code'):
+            if e.status_code == 429:
+                await channel.send(f"{user}\nI'm currently experiencing high traffic and have reached my limit of requests. Please try again later.")
+            else:
+                await channel.send(f"{user}\nI encountered an error while processing your message.")
         else:
-            await channel.send(f"{user}\nI encountered an error while processing your message.")
+            await channel.send(f"{user}\nUnexpected error: {str(e)}")
         logging.error(f"An error occurred: {str(e)}")
-
-
 
 
 @bot.tree.command(name="chat", description="Talk with ChatGPT.")
 async def chat(interaction: discord.Interaction, *, message: str):
-    # Only defer if the interaction has not been responded to
-    if not interaction.response.is_done():
-        await interaction.response.defer(ephemeral=True)
-
     user = interaction.user.mention
     channel = interaction.channel
+    
+    # Let the user know the message has been processed
+    await interaction.response.send_message("Message processed.", ephemeral=True, delete_after=3)
 
-    # Process the chat
-    await process_chat(user, message, channel)
+    # Show as typing while processing the chat
+    async with channel.typing():
+        response_text = await process_chat(user, message, channel)
 
-    # After processing, edit the original deferred response
-    await interaction.edit_original_response(content="Message processed.")
+    # Send the response as a regular message, but only if it's not None
+    if response_text:
+        await channel.send(f"{user}\n{response_text}")
 
 
 
-# run the bot
+# Run the bot
 if __name__ == '__main__':
     bot.run(discord_token)
